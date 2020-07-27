f2df51e3c59a3a25a5653b8559e95abec9d3a2fe
RussellLuo <luopeng.he@gmail.com>
2020-07-27 20:40:09 +0800

    Update example

--- example_test.go
+++ example_test.go
@@ -8,49 +8,38 @@ import (
 )
 
 func Example() {
+	// Typically located in `func init()` of package a.
 	appx.MustRegister(appx.New("a").
 		Init(func(ctx context.Context, apps map[string]*appx.App) (appx.Value, appx.CleanFunc, error) {
-			fmt.Printf("Initializing app %q, which requires %d app.\n", "a", len(apps))
+			fmt.Printf("Initializing app %q, which requires %d app\n", "a", len(apps))
 			return nil, func() error {
-				fmt.Printf("Doing cleanup for app %q.\n", "a")
+				fmt.Println(`Doing cleanup for app "a"`)
 				return nil
 			}, nil
 		}))
 
+	// Typically located in `func init()` of package b.
 	appx.MustRegister(appx.New("b").
-		Init(func(ctx context.Context, apps map[string]*appx.App) (appx.Value, appx.CleanFunc, error) {
-			fmt.Printf("Initializing app %q, which requires %d app.\n", "b", len(apps))
-			return nil, func() error {
-				fmt.Printf("Doing cleanup for app %q.\n", "b")
-				return nil
-			}, nil
-		}))
-
-	appx.MustRegister(appx.New("c").
 		Require("a").
 		Init(func(ctx context.Context, apps map[string]*appx.App) (appx.Value, appx.CleanFunc, error) {
-			fmt.Printf("Initializing app %q, which requires app %q.\n", "c", apps["a"].Name)
+			fmt.Printf("Initializing app %q, which requires app %q\n", "b", apps["a"].Name)
 			return nil, func() error {
-				fmt.Printf("Doing cleanup for app %q.\n", "c")
+				fmt.Println(`Doing cleanup for app "b"`)
 				return nil
 			}, nil
 		}))
 
-	appx.MustRegister(appx.New("d").
-		Require("a", "b").
-		Init(func(ctx context.Context, apps map[string]*appx.App) (appx.Value, appx.CleanFunc, error) {
-			fmt.Printf("Initializing app %q, which requires app %q and %q.\n", "d", apps["a"].Name, apps["b"].Name)
-			return nil, func() error {
-				fmt.Printf("Doing cleanup for app %q.\n", "d")
-				return nil
-			}, nil
-		}))
-
-	if err := appx.Install(context.Background()); err != nil {
+	// Typically located in `func main()` of package main.
+	if err := appx.Install(context.Background(), "b"); err != nil {
+		fmt.Printf("err: %v\n", err)
+	}
+	if err := appx.Uninstall("a", "b"); err != nil {
 		fmt.Printf("err: %v\n", err)
 	}
 
-	if err := appx.Uninstall(); err != nil {
-		fmt.Printf("err: %v\n", err)
-	}
+	// Output:
+	// Initializing app "a", which requires 0 app
+	// Initializing app "b", which requires app "a"
+	// Doing cleanup for app "a"
+	// Doing cleanup for app "b"
 }
